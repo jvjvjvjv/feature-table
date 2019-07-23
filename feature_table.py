53c8550399c5a68c80946ec23cd1be82bcdab41a
jvjvjvjv <43178493+jvjvjvjv@users.noreply.github.com>
2019-07-23 16:07:00 -0400

    checks first and last codon for start/stop

--- feature_table.py
+++ feature_table.py
@@ -7,16 +7,24 @@ table = input("Translation Table: ")
 fasta = sys.argv[1]
 sequence_record = list(SeqIO.parse(fasta, "fasta"))
 
-for n,i in enumerate(sequence_record):
+for i in sequence_record:
     frame_1 = i.seq.translate(5)
     frame_2 = i.seq[1:].translate(5)
     frame_3 = i.seq[2:].translate(5)
     for m,j in enumerate([frame_1, frame_2, frame_3]):
-        if "*" not in j:
+        if "*" not in j[:-1]:
+            if j[0] is "M":   #if first codon is start
+                start = "1"
+            else:
+                start = "<1"
+            if j[-1] is "*":   #if last codon is stop
+                end = str(len(i.seq))
+            else:
+                end = ">" + str(len(i.seq))
             print(">Feature ", i.name)
-            print("<1", ">"+ str(len(i.seq)), "gene", sep = "\t")
+            print(start, end, "gene", sep = "\t")
             print("","","","gene", gene, sep = "\t")
-            print("<1", ">"+ str(len(i.seq)), "CDS", sep = "\t")
+            print(start, end, "CDS", sep = "\t")
             print("","","", "product", pd, sep = "\t")
             print("","","", "codon_start", m+1, sep = "\t")
             print("","","", "transl_table", table, sep = "\t")
